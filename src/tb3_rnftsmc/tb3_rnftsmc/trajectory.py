bda710b11217561552c2385e29c8a8f384b60fce
tom Moncorge <tm.moncorge@gmail.com>
2025-12-06 20:57:44 +0100

    Ajout des packages ROS 2 au projet

---
+++ src/tb3_rnftsmc/tb3_rnftsmc/trajectory.py
@@ -0,0 +1,15 @@
+# tb3_rnftsmc/trajectory.py
+import numpy as np
+
+def trajectory_reference(t, traj_params):
+    R = traj_params['R_cercle']
+    cx = traj_params['centre_x']
+    cy = traj_params['centre_y']
+    omega = traj_params['omega_r']
+    xd = cx + R * np.cos(omega * t)
+    yd = cy + R * np.sin(omega * t)
+    dxd = -R * omega * np.sin(omega * t)
+    dyd = R * omega * np.cos(omega * t)
+    ddxd = -R * (omega**2) * np.cos(omega * t)
+    ddyd = -R * (omega**2) * np.sin(omega * t)
+    return xd, yd, dxd, dyd, ddxd, ddyd
