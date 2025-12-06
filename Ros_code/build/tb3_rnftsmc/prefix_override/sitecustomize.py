86dd6f130722c0b5976fc0502c90696a0d832d59
tom Moncorge <tm.moncorge@gmail.com>
2025-12-06 21:09:02 +0100

    Nettoyage: Déplacement de la source ROS dans Ros_code/

--- Ros_code/build/tb3_rnftsmc/prefix_override/sitecustomize.py
+++ Ros_code/build/tb3_rnftsmc/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/ubuntu/tb3_ws/install/tb3_rnftsmc'
+    sys.prefix = sys.exec_prefix = '/home/tomtom/Projet-sensor-based-controll/Ros_code/install/tb3_rnftsmc'
